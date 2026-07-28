aa568a7798d15ef302cce2bc9f68e51c330d6f2d
tiandic <2323748675@qq.com>
2026-07-28 16:00:48 +0800

    add ucode

--- auto-script/init-arch-linux.1.sh
+++ auto-script/init-arch-linux.1.sh
@@ -4,16 +4,16 @@ set -euo pipefail
 # 普通用户
 read -p "user name: " user_name
 
-while true ; do
-    read -rsp "set user password: " user_passwd
-    echo
-    read -rsp "Enter password again: " user_passwd2
-    echo
+while true; do
+  read -rsp "set user password: " user_passwd
+  echo
+  read -rsp "Enter password again: " user_passwd2
+  echo
 
-    if [[ "$user_passwd" == "$user_passwd2" ]] ; then
-        break
-    fi
-    echo "The passwords entered twice do not match!"
+  if [[ "$user_passwd" == "$user_passwd2" ]]; then
+    break
+  fi
+  echo "The passwords entered twice do not match!"
 done
 
 pacman -S --noconfirm --needed sudo vi
@@ -25,8 +25,8 @@ sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 # 开启32位源,添加archlinuxcn源
 sed -ie '{N;s/#\(\[multilib\]\n\)#\(Include.*\)/\1\2/}' /etc/pacman.conf
-printf '[archlinuxcn]\nServer = https://repo.archlinuxcn.org/$arch\n' >> /etc/pacman.conf
-pacman -Syu --noconfirm --needed archlinuxcn-keyring 
+printf '[archlinuxcn]\nServer = https://repo.archlinuxcn.org/$arch\n' >>/etc/pacman.conf
+pacman -Syu --noconfirm --needed archlinuxcn-keyring
 
 # 安装AUR助手,字体,音视频,性能模式
 pacman -S --noconfirm --needed base-devel yay noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-jetbrains-mono-nerd sof-firmware alsa-ucm-conf pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack
@@ -40,14 +40,14 @@ systemctl enable --now power-profiles-daemon bluetooth
 btrfs filesystem mkswapfile --size 16g --uuid clear /swap/swapfile
 swapon /swap/swapfile
 
-echo "/swap/swapfile none swap defaults 0 0" >> /etc/fstab
+echo "/swap/swapfile none swap defaults 0 0" >>/etc/fstab
 
-# 显卡
+# 显卡与微码
 cpu_info=$(grep "model name" /proc/cpuinfo | head -1)
-if [[ "$cpu_info" == *"Intel"* ]] ; then
-    pacman -S --noconfirm --needed mesa lib32-mesa vulkan-intel lib32-vulkan-intel
+if [[ "$cpu_info" == *"Intel"* ]]; then
+  pacman -S --noconfirm --needed mesa lib32-mesa vulkan-intel lib32-vulkan-intel intel-ucode
 else
-    pacman -S --noconfirm --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon
+  pacman -S --noconfirm --needed mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon amd-ucode
 fi
 
 # 快照

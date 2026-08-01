bdad125b0fc0eef90f2bd210856411011431ce1c
tiandic <2323748675@qq.com>
2026-08-01 14:57:09 +0800

    Added comments.
    Fixed the issue where it would still try to delete a directory even if there were files inside it.

--- auto-script/optimize.sh
+++ auto-script/optimize.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 
 git clone http://github.com/tiandic/plana-themes
 
+# grub 主题
 cd plana-themes
 sudo cp -r grub/plana /boot/grub/themes/
 sudo sed -n 's#GRUB_THEME=.*#GRUB_THEME="/boot/grub/themes/plana/theme.txt"#' /etc/default/grub
 
+# plymouth 主题
 sudo yay -S --noconfirm plymouth-git
 sudo python3 -c '
 with open("/etc/mkinitcpio.conf") as f:
@@ -30,6 +33,7 @@ with open("/etc/mkinitcpio.conf","w") as f:
 sudo cp -r plymouth/plana /usr/share/plymouth/themes/
 sudo plymouth-set-default-theme -R plana
 
+# sddm 主题
 sudo pacman -S --need sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg 7zip
 7z x sddm/plana/Backgrounds/plana.7z.001 -osddm/plana/Backgrounds/
 sudo cp sddm/plana/Fonts/NotoSansMono-VariableFont_wdth,wght.ttf /usr/share/fonts/ # 安装字体
@@ -39,12 +43,15 @@ Current=plana" | sudo tee /etc/sddm.conf
 echo "[General]
 InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
 
-cp -r fcitx5/OriDark $HOME/.local/share/fcitx5/themes/
+# fcitx5 主题
+cp -r fcitx5/OriDark "$HOME/.local/share/fcitx5/themes/"
 echo "Please manually choose a theme."
 fcitx5-configtool
 
+# hyprlock 主题
 cp hyprlock/* ~/.config/hypr/
 
+cd ..
 rm -rf plana-themes
 
 # dotfiles
@@ -69,6 +76,7 @@ stow zsh
 stow waybar
 stow niri
 
+# 配置 Win键 打开 rofi
 yay -S --noconfirm --needed keyd
 echo "# /etc/keyd/default.conf
 
@@ -81,8 +89,10 @@ overload_tap_timeout = 300
 [main]
 leftmeta = overload(meta, macro(leftmeta+0))" | sudo tee /etc/keyd/default.conf
 
+# 全局中文
 echo "LANG=zh_CN.UTF-8" | sudo tee /etc/locale.conf
 
+# LUKS 尝试解密次数 无限制
 sudo python3 -c '
 with open("/etc/default/grub") as f:
     lines=f.readlines()
@@ -105,5 +115,6 @@ with open("/etc/default/grub","w") as f:
         f.write(line)
 '
 
+# 临时文件目录 (占用磁盘空间, 重启清空)
 echo "D /home/${USER}/tmp 1777 root root -" | sudo tee /etc/tmpfiles.d/mytmp.conf
 sudo systemd-tmpfiles --create

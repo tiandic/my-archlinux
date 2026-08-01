f88ab9ae7b10ef98dc22591b053ae12b3e2822b5
tiandic <2323748675@qq.com>
2026-08-01 14:58:50 +0800

    Added rime configuration.

--- auto-script/optimize.sh
+++ auto-script/optimize.sh
@@ -47,6 +47,48 @@ InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
 cp -r fcitx5/OriDark "$HOME/.local/share/fcitx5/themes/"
 echo "Please manually choose a theme."
 fcitx5-configtool
+cat >"$HOME/.local/share/fcitx5/rime/default.custom.yaml" <<EOF
+patch:
+  # 这里的 rime_ice_suggestion 为雾凇方案的默认预设
+  # 其路径位于 /usr/share/rime-data/rime_ice_suggestion.yaml
+  __include: rime_ice_suggestion:/
+  __patch:
+    # 方案列表
+    schema_list:
+      # 可以直接删除或注释不需要的方案，对应的 *.schema.yaml 方案文件也可以直接删除
+      # 除了 t9，它依赖于 rime_ice，用九宫格别删 rime_ice.schema.yaml
+      - schema: rime_ice               # 雾凇拼音（全拼）
+EOF
+cat >"$HOME/.local/share/fcitx5/rime/rime_ice.custom.yaml" <<EOF
+# 其默认配置文件路径位于 /usr/share/rime-data/rime_ice.schema.yaml
+# 其中一段:
+# switches:
+#  - name: ascii_mode
+#    states: [ 中, Ａ ]
+#  - name: ascii_punct  # 中英标点
+#    states: [ ¥, $ ]
+
+patch:
+  "switches/@1":
+    - name: ascii_punct  # 中英标点
+      reset: 1
+      states: [ ¥, $ ]
+
+  # 关闭 输入 "sj" 等组合时, 候选 会有 "18:13" 的配置
+  "engine/translators":
+    - punct_translator
+    - script_translator
+    # - lua_translator@*date_translator    # 时间、日期、星期
+    - lua_translator@*lunar              # 农历
+    - lua_translator@*uuid               # UUID
+    - table_translator@custom_phrase     # 自定义短语 custom_phrase.txt
+    - table_translator@melt_eng          # 英文输入
+    - table_translator@cn_en             # 中英混合词汇
+    - table_translator@radical_lookup    # 部件拆字反查
+    - lua_translator@*unicode            # Unicode
+    - lua_translator@*number_translator  # 数字、金额大写
+    - lua_translator@*calc_translator    # 计算器
+EOF
 
 # hyprlock 主题
 cp hyprlock/* ~/.config/hypr/
